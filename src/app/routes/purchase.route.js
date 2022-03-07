6a6224516f7722a5e9b1431fa373f90985a26a95
Yuri Souza <yuri.santos@ccc.ufcg.edu.br>
2022-03-07 13:53:09 -0300

    Ajusting role

--- src/app/routes/purchase.route.js
+++ src/app/routes/purchase.route.js
@@ -5,13 +5,13 @@ const router = express.Router();
 const PurchaseController = require('../controllers/purchase.controller');
 
 const permission = require('../middlewares/permission.middleware');
-const { ADMIN, CLIENT } = require('../enums/permission.enum');
+const { CLIENT } = require('../enums/permission.enum');
 
 
-router.get('/', permission(ADMIN, CLIENT), PurchaseController.getAll);
-router.get('/:purchaseId', permission(ADMIN, CLIENT), PurchaseController.getById);
-router.put('/:purchaseId', permission(ADMIN, CLIENT), PurchaseController.update);
-router.delete('/:purchaseId', permission(ADMIN, CLIENT), PurchaseController.removePurchase);
+router.get('/', permission(CLIENT), PurchaseController.getAll);
+router.get('/:purchaseId', permission(CLIENT), PurchaseController.getById);
+router.put('/:purchaseId', permission(CLIENT), PurchaseController.update);
+router.delete('/:purchaseId', permission(CLIENT), PurchaseController.removePurchase);
 
 
 module.exports = router;
--- src/app/routes/purchaseItem.route.js
+++ src/app/routes/purchaseItem.route.js
@@ -5,11 +5,11 @@ const router = express.Router();
 const PurchaseItemController = require('../controllers/purchaseItem.controller');
 
 const permission = require('../middlewares/permission.middleware');
-const { ADMIN, CLIENT } = require('../enums/permission.enum');
+const { CLIENT } = require('../enums/permission.enum');
 
 
-router.post('/item/:itemId/flavor/:flavorId', permission(ADMIN, CLIENT), PurchaseItemController.add);
-router.delete('/item/:itemId/flavor/:flavorId/purchase/:purchaseId', permission(ADMIN, CLIENT), PurchaseItemController.remove);
+router.post('/item/:itemId/flavor/:flavorId', permission(CLIENT), PurchaseItemController.add);
+router.delete('/item/:itemId/flavor/:flavorId/purchase/:purchaseId', permission(CLIENT), PurchaseItemController.remove);
 
 
 module.exports = router;
