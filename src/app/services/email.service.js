df04342196b24a95cdf596b9fd2257272a101fd9
Yuri Souza <yuri.santos@ccc.ufcg.edu.br>
2022-05-16 21:15:18 -0300

    credentials update

--- src/app/services/email.service.js
+++ src/app/services/email.service.js
@@ -5,6 +5,7 @@ require('dotenv/config');
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
+  secure: true,
   auth: {
     user: 'salgadinhos2finos@gmail.com',
     pass: 'salgadinhos1234'
